f430122f9a0d141b27c3e99a8c21fd59bad16ede
Debadyuti Karmakar <debadyuti.karmakar@gmail.com>
2024-05-13 13:41:23 +0530

    design app opening page

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,48 @@
 
 import React from 'react'
-import { Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar'
-import { Link } from 'expo-router'
+import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native';
+
+import { Link, router,Redirect } from 'expo-router'
+import {images} from "../constants"
+import CustomButton from './components/CustomButton';
+import { StatusBar } from 'expo-status-bar';
 
 export default function index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Videoraphy</Text>
-      <StatusBar style='auto' />
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height:'100%'}}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+          source={images.logo}
+          className="w-[130px] h-[84px]"
+          resizeMode="contain"
+          />
+          <Image
+          source={images.cards}
+          className="max-w-[380px] w-full h-[300px]"
+          resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-2xl text-white font-bold text-center">
+              Discover Endless Possibilities With{ " "} 
+              <Text className="text-secondary-200">Videography</Text>
+            </Text>
+          </View>
 
-      <Link href='/home'>Go to home</Link>
-    </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+          The world is watching. Show them what you've got.
+          </Text>
+          <CustomButton
+          title="Continue with Email"
+          handlePress={()=>router.push('/sign-in')}
+          containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar
+      backgroundColor='#161622' style='light'
+      /> 
+    </SafeAreaView>
   )
 }
 
