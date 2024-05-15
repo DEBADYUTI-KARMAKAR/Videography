bbb6ecc2548422e93c9e1a3e8e55c584441dbffe
Debadyuti Karmakar <debadyuti.karmakar@gmail.com>
2024-05-16 02:55:54 +0530

    home screen data fetch

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const EmptyState = () => {
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { images } from "../constants";
+import CustomButton from "./CustomButton"
+import { router } from "expo-router";
+const EmptyState = ({title,subtitle}) => {
   return (
-    <View>
-      <Text>EmptyState</Text>
-    </View>
-  )
-}
+    <View className="justify-center items-center px-4">
+      <Image
+        source={images.empty}
+        className="w-[270px]
+      h-[215px] "
+        resizeMode="contain"
+      />
+      <Text className="font-pmedium text-sm text-gray-100">{subtitle}</Text>
+      <Text className="text-xl text-center mt-2 font-psemibold text-white ">{title}</Text>
+      <CustomButton
+      title="Create Video"
+      handlePress={()=> router.push('/create')}
 
-export default EmptyState
+      containerStyles="w-full my-5"
+      />
+    </View>
+  );
+};
+
+export default EmptyState;
